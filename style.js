c86001236a8700feb2f79f7777c8e5d052898256
Ozge Bahceci <ozge.bahceci@hotmail.com>
2023-02-11 18:30:12 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,11 @@
+`use strict`;
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
+
+
+
